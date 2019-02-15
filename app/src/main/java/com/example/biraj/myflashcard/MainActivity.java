a9eda5b3c00592f284c9a535c9dfb2db0a690c5b
birajsilwal <biraj.silwalhmb2@gmail.com>
2019-02-15 15:51:22 -0700

    i did it

--- app/src/main/java/com/example/biraj/myflashcard/MainActivity.java
+++ app/src/main/java/com/example/biraj/myflashcard/MainActivity.java
@@ -1,30 +1,50 @@
 package com.example.biraj.myflashcard;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-
-    TextView flashcard_question;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        flashcard_question = (TextView) findViewById(R.id.flashcard_question);
-        flashcard_question.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
             }
+
         });
 
+        findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createCard = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(createCard,100);
+                //startActivity(createCard);
+            }
+        });
+    }
 
-    }};
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            //Intent intent = getIntent();
+            String question = data.getStringExtra("question");
+            String answer = data.getStringExtra("answer");
+
+
+            TextView flashcard_question = (TextView) findViewById(R.id.flashcard_question);
+            TextView flashcard_answer = (TextView) findViewById(R.id.flashcard_answer);
+
+            flashcard_question.setText(question);
+            flashcard_answer.setText(answer);
+    }
+}
 
